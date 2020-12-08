19ba4855f9617ed975947fd675e6eb03d8cdfa99
mindruttoma <mindrut.toma@gmail.com>
2020-12-08 17:23:50 +0200

    update item13

--- src/LibraryFactory.cpp
+++ src/LibraryFactory.cpp
@@ -1,7 +1,16 @@
 #include "LibraryFactory.h"
 #include "Library.h"
+
 using namespace std;
 Library* LibraryFactory ::createLibrary(){
     tr1::shared_ptr <Library> pLibrary(createLibrary()); // ITEM 13 -> AUTO RELEASE OBJECT BY DESTRUCTOR WHEN IT IS USELESS (MEMORY LEAK)
+    return *pLibrary;
+}
+
+Library* LibraryFactory :: createLibrary(vector <Book> books){
+    tr1::shared_ptr <Library> pLibrary(createLibrary()); // ITEM 13 -> AUTO RELEASE OBJECT BY DESTRUCTOR WHEN IT IS USELESS (MEMORY LEAK)
+    for(int i=0;i<this->books.size();i++){
+        pLibrary.addBook(books[i]);
+    }
     return *pLibrary;
 }
